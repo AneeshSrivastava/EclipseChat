39845ab819d3dde065f3128275107ca45ecb28e0
Aneesh Srivastava <srivastavaaneesh004@gmail.com>
2024-08-18 23:02:43 +0530

    feat: Add message and user schemas and, user models

---
+++ eclipsechat/src/model/interfaces/schemas/user.ts
@@ -0,0 +1,13 @@
+import { Document } from "mongoose";
+import { Message } from "./message";
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  messages: Message[];
+}
---
+++ eclipsechat/src/model/user.ts
@@ -0,0 +1,59 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+import { Message } from "./interfaces/schemas/message";
+import { User } from "./interfaces/schemas/user";
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..t/, "Please use a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify Code is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify Code Expiry is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    required: true,
+  },
+  messages: [MessageSchema],
+});
+
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
+   
